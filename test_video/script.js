2d1dfe99d5f224e0e10593eaa2cdb0b587b54aad
jingyu-f <79663022+jingyu-f@users.noreply.github.com>
2021-11-04 12:17:09 -0400

    s

--- test_video/script.js
+++ test_video/script.js
@@ -9,7 +9,7 @@ var now = time * 60 + date.getMinutes();
 console.log(now);
 if (now >= 600 && now <= 800) {
 //document.querySelector('#bela_fleck').autoplay();
-document.getElementById("bela_fleck").src += "?&autoplay=1";
+document.getElementById("bela_fleck").src += "?&&autoplay=1";
 }
 
 })
