d56a622ad22cb7e14d89fc7d6fb281e9b89d8da3
jingyu-f <79663022+jingyu-f@users.noreply.github.com>
2021-11-04 11:05:29 -0400

    test

---
+++ test_video/script.js
@@ -0,0 +1,8 @@
+
+ $(document).ready(function(){
+console.log('script loaded');
+
+document.getElementById('bela_fleck').style = "display: none";
+document.getElementById('bela_fleck').src += "?&autoplay=1";
+
+})
