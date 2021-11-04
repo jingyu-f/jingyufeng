489008840151f21c492b3e45d20a4d361c52452b
jingyu-f <79663022+jingyu-f@users.noreply.github.com>
2021-11-04 11:09:19 -0400

    Update script.js

--- test_video/script.js
+++ test_video/script.js
@@ -2,7 +2,7 @@
  $(document).ready(function(){
 console.log('script loaded');
 
-document.getElementById('bela_fleck').style = "display: none";
+//document.getElementById('bela_fleck').style = "display: none";
 document.getElementById('bela_fleck').src += "?&autoplay=1";
 
 })
