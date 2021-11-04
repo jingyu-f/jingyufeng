eba3a7f6bcc0929258433ee2964c8d7d38d1c405
jingyu-f <79663022+jingyu-f@users.noreply.github.com>
2021-11-04 11:48:49 -0400

    s

--- test_video/script.js
+++ test_video/script.js
@@ -2,7 +2,15 @@
  $(document).ready(function(){
 console.log('script loaded');
 
-//document.getElementById('bela_fleck').style = "display: none";
-//document.getElementById('bela_fleck').src += "?&autoplay=1";
+var date = new Date();
+var time = date.getHours();
+var now = time * 60 + date.getMinutes();
+
+console.log(now);
+if (now >= 600 && now <= 800) {
+//document.querySelector('#bela_fleck').autoplay();
+document.getElementById('bela_fleck').src += "?&autoplay=1";
+//$("#bela_fleck").src += "?&autoplay=1";
+}
 
 })
