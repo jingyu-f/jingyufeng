15a1821a1809478459e852405fccd4c10f311963
jingyu-f <79663022+jingyu-f@users.noreply.github.com>
2022-09-30 11:07:11 -0400

    test

--- independent_project/index.php
+++ independent_project/index.php
@@ -1,16 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!--<link rel="stylesheet" href="test.css">-->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-        <script src="script.js"></script>
-
-
+<title>living web</title>
 </head>
 <body>
+    <?php echo '<h1>hello</h1>' ?>
     <form action="upload.php" method="post" enctype="multipart/form-data">
     <input type = "file" name = "my_image">
     <input type = "submit" name = "submit" value = "Upload">
@@ -18,4 +12,5 @@
     
             
 </body>
+
 </html>
