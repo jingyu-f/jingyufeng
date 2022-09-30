76774b01f080ac2fb2d3bfc8ab4886570817770e
jingyu-f <79663022+jingyu-f@users.noreply.github.com>
2022-09-30 10:41:29 -0400

    isp

---
+++ independent_project/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--<link rel="stylesheet" href="test.css">-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script src="script.js"></script>
+
+
+</head>
+<body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type = "file" name = "my_image">
+    <input type = "submit" name = "submit" value = "Upload">
+</form> 
+    
+            
+</body>
+</html>
---
+++ independent_project/upload.php
@@ -0,0 +1,8 @@
+<?php
+
+    if (isset($_POST['submit']) && isset($_FILES['my_image'])){
+        echo "Hello";
+    }else{
+        header('Location: index.php');
+
+    }
